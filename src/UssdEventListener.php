f94711c958465d36e971cb0e7133b1e5e97a4208
David Bwire <israelbwire@gmail.com>
2017-08-14 01:28:58 +0300

    directly pass $ussdMenusConfig when instantiating UssdEventListener

--- src/UssdEventListener.php
+++ src/UssdEventListener.php
@@ -25,37 +25,11 @@ class UssdEventListener extends SharedEventManager {
 
     /**
      * 
-     * @param UssdMenuConfig $ussdMenuConfig
+     * @param array $ussdMenusConfig
      */
-    public function __construct(UssdMenuConfig $ussdMenuConfig) {
-
-        $ussdMenusConfig             = $ussdMenuConfig->getUssdMenus();
+    public function __construct(array $ussdMenusConfig) {
         $this->attachSystemEvents($ussdMenusConfig);
         $this->ussdResponseGenerator = new UssdResponseGenerator();
-
-        // HOME PAGE example
-        // example - attaching an event
-        $this->attach(__NAMESPACE__, 'home_instant_ussd', function($e) use ($ussdMenusConfig) {
-            if (!$e instanceof UssdEvent) {
-                return false;
-            }
-            $menuConfig = $ussdMenusConfig[$e->getName()];
-            if (!$e->containsIncomingData()) {
-                $this->attachDynamicErrors($e, $menuConfig);
-                // clear tracked menus
-                $isValidResponse = $e->getParam('is_valid', true);
-                if ($isValidResponse) {
-                    $this->clearMenuVisitHistory($e);
-                }
-                return $this->ussdResponseGenerator->composeAndRenderUssdMenu($menuConfig, true, false);
-            }
-            // we have data sent in
-            // 1. Get latest response & save it; as it should be valid
-            $latestResponse = $e->getLatestResponse();
-            $lastServedMenu = $e->getName();
-            // 2. Determine next menu using latest response
-            return $this->ussdResponseGenerator->determineNextMenu($lastServedMenu, $menuConfig, $latestResponse);
-        });
     }
 
     /**
