342ed2019b2b87f33145cb29ef2ca27ce73f8463
David Bwire <israelbwire@gmail.com>
2018-07-11 15:32:59 +0300

    add DefaultScreenListener

---
+++ src/Listeners/DefaultScreenListener.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Bitmarshals\InstantUssd\Listeners;
+
+use Bitmarshals\InstantUssd\Listeners\UssdEventListener;
+
+/**
+ * Description of DefaultScreenListener. Used to easily set up screen listeners
+ *
+ * @author David Bwire <israelbwire@gmail.com>
+ */
+class DefaultScreenListener extends UssdEventListener {
+
+    public static function configure() {
+        
+    }
+
+    protected function captureIncomingData() {
+        
+    }
+
+}
